84ec023d281909d2790e73f3a7ed0bed085ca574
Evan McGrath <evan.grath@gmail.com>
2023-12-25 17:47:29 -0500

    refactor: clean up server file

--- server.js
+++ server.js
@@ -1,45 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose')
 const app = express();
 const PORT = 3100
 const cors = require('cors')
-const SpotifyWebApi = require('spotify-web-api-node');
 
+// Middleware 
 app.use(express.json());
 app.use(cors());
 
-
-// mongoose.connect('mongodb://127.0.0.1:27017/jamifydb');
-
-
-
-
-
+// Routes
 const loginRoute = require('./routes/loginRoute')
 app.use('/login', loginRoute);
-
-
 
 const userInfoRoute = require('./routes/userInfoRoute')
 app.use('/userInfo', userInfoRoute);
 
-
-
 const playlistInfo = require('./routes/playlistInfoRoute')
 app.use('/playlist', playlistInfo)
-
-
-
 
 const songInfo = require('./routes/songInfo')
 app.use('/song', songInfo)
 
-
-
-
-
-
-
+// Initialize server 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
